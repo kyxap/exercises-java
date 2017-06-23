a98e7141a5bc86560c9d37598d5fb52ec8af66e4
Roman Kukharuk <rkukharuk@gmail.com>
2017-06-22 22:58:22 -0700

    nothing interested

---
+++ collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -0,0 +1,19 @@
+class CollatzCalculator {
+
+    int computeStepCount(int start) {
+        if (start <= 0) {
+            throw new IllegalArgumentException("Only natural numbers are allowed");
+        }
+        int count = 0;
+        while (start > 1){
+            if (start % 2 == 0) {
+                start /= 2;
+            } else {
+                start = start * 3 + 1;
+            }
+            count++;
+        }
+        return count;
+    }
+
+}
