764661fb31a84f5dc13ac43e611a3bf8d0cc7f21
Roman Kukharuk <rkukharuk@gmail.com>
2017-07-21 11:34:38 -0700

    can add some refactor in feature

---
+++ rotational-cipher/src/main/java/RotationalCipher.java
@@ -0,0 +1,36 @@
+public class RotationalCipher {
+    int rotation = 0;
+    int max = (int)'z' - (int)'a' + 1;
+
+    public RotationalCipher(int i) {
+        while (i - max >= 0) {
+            i = i - max;
+        }
+        this.rotation = i;
+    }
+
+    public String rotate(String str) {
+        String rez = "";
+        for (char c : str.toCharArray()) {
+            if (c >= 'A') {
+                int biggest = findMax(c);
+                if ((int)c + rotation > biggest){
+                    rez += (char)(biggest - max + ((int) c + rotation - biggest));
+                } else {
+                    rez+= (char)((int) c + rotation);
+                }
+            } else {
+                rez+= c;
+            }
+        }
+        return rez;
+    }
+
+    int findMax(char c) {
+        if (c >= 'A' && c <= 'Z') {
+           return (int)'Z';
+        } else if (c >= 'a' && c <= 'z'){
+            return (int) 'z';
+        } else return 127;
+    }
+}
